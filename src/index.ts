727cdd1590a089945732ec7a3ad52a2076265688
yue7872 <2258462872@qq.com>
2022-12-08 17:56:43 +0800

    feat: v0.0

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,29 @@
-import { window } from 'vscode'
+import { exec } from 'child_process'
+import type { ExtensionContext } from 'vscode'
+import { commands, window, workspace } from 'vscode'
 
-export function activate() {
-  window.showInformationMessage('Hello')
+export function activate(context: ExtensionContext) {
+  const disposable = commands.registerCommand('open-git', () => {
+    const openWindow = window.createTerminal('gitOpen')
+    const projectRoot = workspace.workspaceFolders![0].uri.path
+
+    exec(`cd ${projectRoot} && git config --get remote.origin.url`, (err, stdout) => {
+      if (err) {
+        window.showInformationMessage(err.message)
+        return
+      }
+
+      // ssh地址转换为https地址
+      const gitUrl = stdout.replace('.git', '').replace('.com:', '.com/').replace('git@', 'https://')
+
+      openWindow.sendText(`open ${gitUrl}`)
+      openWindow.show()
+      window.showInformationMessage('已在浏览器打开git仓库')
+    })
+  })
+
+  context.subscriptions.push(disposable)
 }
-
 export function deactivate() {
 
 }
